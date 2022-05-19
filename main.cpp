bd127fa59713b71283dc5db57db7c8f0f857b06c
jzhoujg <774780180@qq.com>
2022-05-19 14:51:52 +0800

    0519_cor

--- main.cpp
+++ main.cpp
@@ -22,19 +22,16 @@ using namespace std;
 double dabs(double c)
 
 {
-
 	if (c >= 0) return c;
-	return -1 * c;
-
-
+	return -1.0 * c;
 }
 
 
 
-double* getWeight(double c, double a = -0.55)
+double* getWeight(double c, double a = -0.5)
 {
 	//c就是u和v，横坐标和纵坐标的输出计算方式一样
-	float temp[4];
+	double temp[4];
 
 	temp[0] = c + 1 ; 
 	temp[1] = c;
@@ -45,18 +42,23 @@ double* getWeight(double c, double a = -0.55)
 
 	//y(x) = (a+2)|x|*|x|*|x| - (a+3)|x|*|x| + 1   |x|<=1
 	//y(x) = a|x|*|x|*|x| - 5a|x|*|x| + 8a|x| - 4a  1<|x|<2
+
+	double t = 0.95;
+
 	double weight[4];
-	weight[0] = (a * pow(dabs(temp[0]), 3) - 5 * a * pow(dabs(temp[0]), 2) + 8 * a * dabs(temp[0]) - 4 * a);
-	weight[1] = (a + 2) * pow(dabs(temp[1]), 3) - (a + 3) * pow(dabs(temp[1]), 2) + 1 ;
-	weight[2] = (a + 2) * pow(dabs(temp[2]), 3) - (a + 3) * pow(dabs(temp[2]), 2) + 1 ;
-	weight[3] = (a * pow(dabs(temp[3]), 3) - 5 * a * pow(dabs(temp[3]), 2) + 8 * a * dabs(temp[3]) - 4 * a);
 
 
+	weight[0] = t*(a * pow(dabs(temp[0]), 3) - 5 * a * pow(dabs(temp[0]), 2) + 8 * a * dabs(temp[0]) - 4 * a);
+	weight[1] = t * (a + 2) * pow(dabs(temp[1]), 3) - (a + 3) * pow(dabs(temp[1]), 2) + 1 ;
+	weight[2] = t * (a + 2) * pow(dabs(temp[2]), 3) - (a + 3) * pow(dabs(temp[2]), 2) + 1 ;
+	weight[3] = t * (a * pow(dabs(temp[3]), 3) - 5 * a * pow(dabs(temp[3]), 2) + 8 * a * dabs(temp[3]) - 4 * a);
+
+	// double sum = pow(weight[0], 2) + pow(weight[1], 2) + pow(weight[2], 2) + pow(weight[3], 2);
 
 
+	double sum = weight[0] + weight[1] + weight[2] + weight[3];
 
-
-	// printf("%f \t", float(weight[2]));
+	//printf("%f \t", sum);
 
 	return weight;
 }
@@ -110,6 +112,7 @@ void bicubic_inter(bmp_img in, uint32_t width_in, uint32_t height_in, bmp_img* o
 		
 		for (x = 0; x < width_out; x++)
 		{
+
 			double src_x = (x + 0.5) / 4 + 0.25;
 			size_t x1 = floor((x + 0.5) / 4 + 0.25) + 1;
 
@@ -166,7 +169,7 @@ void bicubic_inter(bmp_img in, uint32_t width_in, uint32_t height_in, bmp_img* o
 	printf("Stage 3(OUTPUT) : START...\n");
 
 
-	bmp_img_write(out, "results/1.bmp");
+	bmp_img_write(out, "results/new.bmp");
 
 
 
@@ -236,6 +239,14 @@ void* multi_inter(void* vargp)
 	double* X;
 	double u_y, u_x;
 
+
+
+
+	double temp_r[2160][3840];
+	double temp_g[2160][3840];
+	double temp_b[2160][3840];
+
+
 	for (size_t y = 0, x; y < height_out; y++)
 	{
 		//double src_y = (y + 0.5) / 4 -0.5 ;
@@ -261,6 +272,8 @@ void* multi_inter(void* vargp)
 
 		Y = getWeight(u_y);
 
+
+
 		for (x = 0; x < width_out; x++)
 		{
 			double src_x = (x + 0.5) / 4 + 0.25;
@@ -279,34 +292,60 @@ void* multi_inter(void* vargp)
 			u_x = src_x - double(x1) + 1;
 			X = getWeight(u_x);
 
+			/*
 			out_p->img_pixels[y][x].red = Y[0] * X[0] * img_p.img_pixels[y0][x0].red + Y[0] * X[1] * img_p.img_pixels[y0][x1].red + Y[0] * X[2] * img_p.img_pixels[y0][x2].red + Y[0] * X[3] * img_p.img_pixels[y0][x3].red
 				+ Y[1] * X[0] * img_p.img_pixels[y1][x0].red + Y[1] * X[1] * img_p.img_pixels[y1][x1].red + Y[1] * X[2] * img_p.img_pixels[y1][x2].red + Y[1] * X[3] * img_p.img_pixels[y1][x3].red
 				+ Y[2] * X[0] * img_p.img_pixels[y2][x0].red + Y[2] * X[1] * img_p.img_pixels[y2][x1].red + Y[2] * X[2] * img_p.img_pixels[y2][x2].red + Y[2] * X[3] * img_p.img_pixels[y2][x3].red
 				+ Y[3] * X[0] * img_p.img_pixels[y3][x0].red + Y[3] * X[1] * img_p.img_pixels[y3][x1].red + Y[3] * X[2] * img_p.img_pixels[y3][x2].red + Y[3] * X[3] * img_p.img_pixels[y3][x3].red
 				;
+*/
 
-			if (out_p->img_pixels[y][x].red > up) out_p->img_pixels[y][x].red = 255;
-			if (out_p->img_pixels[y][x].red < down) out_p->img_pixels[y][x].red = 0;
+			temp_r[y][x] =
+				double(Y[0] * X[0] * img_p.img_pixels[y0][x0].red) + double(Y[0] * X[1] * img_p.img_pixels[y0][x1].red) + double(Y[0] * X[2] * img_p.img_pixels[y0][x2].red) + double(Y[0] * X[3] * img_p.img_pixels[y0][x3].red)
+				+ double(Y[1] * X[0] * img_p.img_pixels[y1][x0].red) + double(Y[1] * X[1] * img_p.img_pixels[y1][x1].red) + double(Y[1] * X[2] * img_p.img_pixels[y1][x2].red) + double(Y[1] * X[3] * img_p.img_pixels[y1][x3].red)
+				+ double(Y[2] * X[0] * img_p.img_pixels[y2][x0].red) + double(Y[2] * X[1] * img_p.img_pixels[y2][x1].red) + double(Y[2] * X[2] * img_p.img_pixels[y2][x2].red) + double(Y[2] * X[3] * img_p.img_pixels[y2][x3].red)
+				+ double(Y[3] * X[0] * img_p.img_pixels[y3][x0].red) + double(Y[3] * X[1] * img_p.img_pixels[y3][x1].red) + double(Y[3] * X[2] * img_p.img_pixels[y3][x2].red) + double(Y[3] * X[3] * img_p.img_pixels[y3][x3].red)
+				;
 
 
+			if (temp_r[y][x] > 255) temp_r[y][x] = 255;
+			if (temp_r[y][x] < 0) temp_r[y][x] = 0;
+
+			out_p->img_pixels[y][x].red = unsigned char(temp_r[y][x]);
+
+
+			temp_b[y][x] = 
+				double(Y[0] * X[0] * img_p.img_pixels[y0][x0].blue) + double(Y[0] * X[1] * img_p.img_pixels[y0][x1].blue) + double(Y[0] * X[2] * img_p.img_pixels[y0][x2].blue) + double(Y[0] * X[3] * img_p.img_pixels[y0][x3].blue)
+				+ double(Y[1] * X[0] * img_p.img_pixels[y1][x0].blue) + double(Y[1] * X[1] * img_p.img_pixels[y1][x1].blue) + double(Y[1] * X[2] * img_p.img_pixels[y1][x2].blue) + double(Y[1] * X[3] * img_p.img_pixels[y1][x3].blue)
+				+ double(Y[2] * X[0] * img_p.img_pixels[y2][x0].blue) + double(Y[2] * X[1] * img_p.img_pixels[y2][x1].blue) + double(Y[2] * X[2] * img_p.img_pixels[y2][x2].blue) + double(Y[2] * X[3] * img_p.img_pixels[y2][x3].blue)
+				+ double(Y[3] * X[0] * img_p.img_pixels[y3][x0].blue) + double(Y[3] * X[1] * img_p.img_pixels[y3][x1].blue) + double(Y[3] * X[2] * img_p.img_pixels[y3][x2].blue) + double(Y[3] * X[3] * img_p.img_pixels[y3][x3].blue);
+			/*
 			out_p->img_pixels[y][x].blue = Y[0] * X[0] * img_p.img_pixels[y0][x0].blue + Y[0] * X[1] * img_p.img_pixels[y0][x1].blue + Y[0] * X[2] * img_p.img_pixels[y0][x2].blue + Y[0] * X[3] * img_p.img_pixels[y0][x3].blue
 				+ Y[1] * X[0] * img_p.img_pixels[y1][x0].blue + Y[1] * X[1] * img_p.img_pixels[y1][x1].blue + Y[1] * X[2] * img_p.img_pixels[y1][x2].blue + Y[1] * X[3] * img_p.img_pixels[y1][x3].blue
 				+ Y[2] * X[0] * img_p.img_pixels[y2][x0].blue + Y[2] * X[1] * img_p.img_pixels[y2][x1].blue + Y[2] * X[2] * img_p.img_pixels[y2][x2].blue + Y[2] * X[3] * img_p.img_pixels[y2][x3].blue
 				+ Y[3] * X[0] * img_p.img_pixels[y3][x0].blue + Y[3] * X[1] * img_p.img_pixels[y3][x1].blue + Y[3] * X[2] * img_p.img_pixels[y3][x2].blue + Y[3] * X[3] * img_p.img_pixels[y3][x3].blue;
-			if (out_p->img_pixels[y][x].blue > up) out_p->img_pixels[y][x].blue = 255;
-			if (out_p->img_pixels[y][x].blue < down) out_p->img_pixels[y][x].blue = 0;
+			*/
 
+			if (temp_b[y][x]> up) temp_b[y][x] = 255;
+			if (temp_b[y][x]< down)temp_b[y][x] =  0;
 
+			out_p->img_pixels[y][x].blue = temp_b[y][x];
 
-			out_p->img_pixels[y][x].green = Y[0] * X[0] * img_p.img_pixels[y0][x0].green + Y[0] * X[1] * img_p.img_pixels[y0][x1].green + Y[0] * X[2] * img_p.img_pixels[y0][x2].green + Y[0] * X[3] * img_p.img_pixels[y0][x3].green
-				+ Y[1] * X[0] * img_p.img_pixels[y1][x0].green + Y[1] * X[1] * img_p.img_pixels[y1][x1].green + Y[1] * X[2] * img_p.img_pixels[y1][x2].green + Y[1] * X[3] * img_p.img_pixels[y1][x3].green
-				+ Y[2] * X[0] * img_p.img_pixels[y2][x0].green + Y[2] * X[1] * img_p.img_pixels[y2][x1].green + Y[2] * X[2] * img_p.img_pixels[y2][x2].green + Y[2] * X[3] * img_p.img_pixels[y2][x3].green
-				+ Y[3] * X[0] * img_p.img_pixels[y3][x0].green + Y[3] * X[1] * img_p.img_pixels[y3][x1].green + Y[3] * X[2] * img_p.img_pixels[y3][x2].green + Y[3] * X[3] * img_p.img_pixels[y3][x3].green;
-			if (out_p->img_pixels[y][x].green > up) out_p->img_pixels[y][x].green = 255;
-			if (out_p->img_pixels[y][x].green < down) out_p->img_pixels[y][x].green = 0;
+			temp_g[y][x] =
+				double(Y[0] * X[0] * img_p.img_pixels[y0][x0].green) + double(Y[0] * X[1] * img_p.img_pixels[y0][x1].green) + double(Y[0] * X[2] * img_p.img_pixels[y0][x2].green) + double(Y[0] * X[3] * img_p.img_pixels[y0][x3].green)
+				+ double(Y[1] * X[0] * img_p.img_pixels[y1][x0].green) + double(Y[1] * X[1] * img_p.img_pixels[y1][x1].green) + double(Y[1] * X[2] * img_p.img_pixels[y1][x2].green) + double(Y[1] * X[3] * img_p.img_pixels[y1][x3].green)
+				+ double(Y[2] * X[0] * img_p.img_pixels[y2][x0].green) + double(Y[2] * X[1] * img_p.img_pixels[y2][x1].green) + double(Y[2] * X[2] * img_p.img_pixels[y2][x2].green) + double(Y[2] * X[3] * img_p.img_pixels[y2][x3].green)
+				+ double(Y[3] * X[0] * img_p.img_pixels[y3][x0].green) + double(Y[3] * X[1] * img_p.img_pixels[y3][x1].green) + double(Y[3] * X[2] * img_p.img_pixels[y3][x2].green) + double(Y[3] * X[3] * img_p.img_pixels[y3][x3].green);
+			
+			
+			if (temp_g[y][x] > up) temp_g[y][x] = 255;
+			if (temp_g[y][x] < down) temp_g[y][x] = 0;
+
+			out_p->img_pixels[y][x].green = temp_g[y][x];
 
 
 		}
+
 		printf("Processing rows %d ... \n", int(y));
 
 	}
@@ -320,7 +359,7 @@ void* multi_inter(void* vargp)
 
 	char res[20] = "1.bmp";
 
-	res[0] = symbol[s];
+	// res[0] = symbol[s];
 	bmp_img_write(out_p, res);
 
 
@@ -374,18 +413,12 @@ int main()
 	bmp_img_free(&img_out);
 	bmp_img_free(&img_in);
 
-
 	
 	printf("exit!\n");
 
 
-	printf("start mul!");
-
-	multi_set(4);
-
-
-	
-
+	//printf("start mul!");
+	// multi_set(4);
 
 
 	return 0;
